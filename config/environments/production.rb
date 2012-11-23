2fe710c9395358bdfa155c637cdc7b7a3bb63a9a
liran <liran.yadlin@gmail.com>
2012-11-23 21:04:18 +0200

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,8 @@ SampleApp::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+
   # Force all access to the app over SSL, use Strict-Transport-Security, 
   # and use secure cookies.
   config.force_ssl = true
-  .
 end
